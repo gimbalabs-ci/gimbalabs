430c09f31cd24d8feea506c192e1730a9c92dcf8
James Dunseith <info@workshopmaybe.com>
2021-02-18 23:52:24 -0500

    Fix import

--- pages/dandelionapis/[dapi].js
+++ pages/dandelionapis/[dapi].js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/Image'
+import Image from 'next/image'
 import dandelion from '../../data/dandelion.json'
 import { TitleHeading1, TitleHeading2, TitleHeading3, HighlightText, CallOutText, Paragraph, CardHeading, CardSubHeading, CardText } from '../../components/Type'
 
