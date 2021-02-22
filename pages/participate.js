43ee76f5694703934a98042895220729d7dc878a
James Dunseith <info@workshopmaybe.com>
2021-02-21 20:48:49 -0500

    Add Discord link

--- pages/participate.js
+++ pages/participate.js
@@ -8,6 +8,9 @@ function participate() {
             <TitleHeading3>Ready to get involved?</TitleHeading3>
             <Paragraph>GimbaLabs is a collaborative platform for people who want to show the world what Cardano can do. We are creating free and open source tools, lessons, and resources that people can learn from.</Paragraph>
             <Paragraph>This week we launched the first Gimbalabs Initiatives. These experiments are helping to create models for how Gimbalabs operates. <Link href="/gli20210211.pdf">Here's a document</Link> outlining the first two experiments. The next round launches on March 8. Look for details about how to get involved on Discord, in Weekly Updates, or on our mailing list.</Paragraph>
+            <br />
+            <a href="https://discord.gg/xS3YmaruJu" className="m-3 text-xl text-purple-500">Join Gimbalabs on Discord</a>
+            
 
             <TitleHeading3>Technical Planning and Educational Marketing</TitleHeading3>
             <Paragraph>Not sure exactly what tools you'll need for your project? Just ask. We're a network of builders who can help you get started.</Paragraph> 
