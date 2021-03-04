6ee323a8c3d6b258fdbe67d4a126a9e3c8b95037
James Dunseith <info@workshopmaybe.com>
2021-03-04 07:02:47 -0500

    Write Tinker copy

--- pages/tinker/codingonchain.js
+++ pages/tinker/codingonchain.js
@@ -8,13 +8,13 @@ export default function liftwallet() {
             <TinkerLayout>
                 <CardHeading>Coding On Chain</CardHeading>
                 <Paragraph>
-                    <a href="https://www.youtube.com/channel/UChp9R55VgwkjMzGP9qMa66g">View on YouTube</a>                
+                    We know you're excited to build on Plutus and Marlowe - so are we! With that in mind, we'd like to tell you a secret: there's a lot you can already build on Cardano right now. With Coding on Chain, we go live and tinker. Each week we start with some sort of "what if...?" or "how could we...?" scenario and then get to hacking. It's a blast.
                 </Paragraph>
                 <Paragraph>
-                    <a href="https://www.twitch.tv/codingonchain">view on Twitch</a>                
+                    Coding on Chain goes <a href="https://www.twitch.tv/codingonchain" className="classic">live on Twitch</a> every Thursday at 4pm GMT / 11am EST.                
                 </Paragraph>
                 <Paragraph>
-                    Live-streaming calendar coming soon!
+                    You can also catch up with the archive <a href="https://www.youtube.com/channel/UChp9R55VgwkjMzGP9qMa66g" className="classic">on YouTube</a>                
                 </Paragraph>
             </TinkerLayout>
         </>
--- pages/tinker/liftwallet.js
+++ pages/tinker/liftwallet.js
@@ -6,8 +6,15 @@ export default function liftwallet() {
     return (
         <>
             <TinkerLayout>
-                <CardHeading>LIFT Wallet</CardHeading>    
+                <CardHeading>LIFT Wallet</CardHeading> 
+                <Paragraph>
+                <a href="https://github.com/CodingOnChain/lift-wallet" className="classic">LIFT Wallet</a> pushes our understanding of what a "wallet" is in the first place. If one goal of Cardano is mainstream adoption, we're going to need all sorts of different wallets that serve different needs, and in some cases even shield the end user from knowing they're relying on a blockchain in the first place.
+            </Paragraph>
+            <Paragraph>
+                Developed by a growing team - and often developed live on Coding on Chain, LIFT Wallet is tinkerers, builders, and everyone who wants to understand more deeply how Cardano works.
+            </Paragraph>   
             </TinkerLayout>
+            
         </>
         
     )
