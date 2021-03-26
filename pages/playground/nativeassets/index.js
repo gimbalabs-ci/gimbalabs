19584c554101eb4fb60839a6f9072687f77c16ab
James Dunseith <jdunseith@gimbalabs.io>
2021-03-26 06:09:38 -0400

    Initialize native assets section in playground

---
+++ pages/playground/nativeassets/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import { TitleHeading1, Paragraph } from '../../../components/Type'
+
+export default function nativeassets() {
+    return (
+        <div className="p-12">
+            <TitleHeading1>
+                Exploring Transactions and Native Assets on Cardano
+            </TitleHeading1>
+            <Paragraph>
+                Running experiments
+            </Paragraph>
+        </div>
+    )
+}
