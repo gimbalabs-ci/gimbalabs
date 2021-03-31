b5db9ae5d7d7e20138d8a5d5e2e85cc83d172714
James Dunseith <jdunseith@gimbalabs.io>
2021-03-31 06:31:09 -0400

    Add interactivity

--- pages/participate/index.js
+++ pages/participate/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { TitleHeading1, TitleHeading2, Paragraph, TitleHeading3 } from '../../components/Type'
-import { CalendarButton, DiscordButton } from '../../components/Buttons'
+import { CalendarButton, DiscordButton, GitlabButton } from '../../components/Buttons'
 
 export default function participate() {
     return (
@@ -9,7 +9,11 @@ export default function participate() {
             <section>
                 <TitleHeading2>Ready to get involved?</TitleHeading2>
                 <Paragraph>GimbaLabs is a collaborative platform for people who want to show the world what Cardano can do. We are creating free and open source tools, lessons, and resources that people can learn from.</Paragraph>
-                <CalendarButton />
+                <div className="flex flex-col md:flex-row justify-around w-full md:w-2/3 mx-auto">
+                    <CalendarButton />
+                    <GitlabButton />
+                    <DiscordButton />
+                </div>
             </section>
             
             
@@ -20,23 +24,25 @@ export default function participate() {
                 </Paragraph>
                 <TitleHeading3>Experiment #1: Short Video Content</TitleHeading3>
                 <Paragraph>
-                    We get it that so much of the online world runs on short video content. We also know that short videos are not really enough to facilitate deep understanding of complicated ideas. So with this experiment, we're asking: "How can we best leverage short video content to encourage a problem solving mindset?" Check out this call to action from Matthew, one of the leaders of this initiative:
+                    We get it that so much of the online world runs on short video content. We also know that short videos are not really enough to facilitate deep understanding of complicated ideas. So with this experiment, we're asking: "How can we best leverage short video content to encourage a problem solving mindset?" <a href="https://youtu.be/XGl7YKVnqN0" className="classic">Check out this call to action from Matthew</a>, one of the leaders of this initiative:
                 </Paragraph>
-                <div className="w-1/2 mx-auto">
+                <div className="w-1/2 mx-auto hidden md:contents">
                        <iframe width="560" height="315" src="https://www.youtube.com/embed/XGl7YKVnqN0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <Paragraph>
                     As you can see, in addition to creating real outcomes, these experiments are helping to create models for how Gimbalabs operates. We've been thrilled to see how new leaders are emerging within our community and will continue to post outcomes and opportunities here.
                 </Paragraph>
                 <TitleHeading3>Experiment #2: Token-based incentive structures</TitleHeading3>
-                <Paragraph>
-                    This experiment generated a lot of great conversations, and it is still a work in progress. Token-based incentive schemes should be taken seriously, and we hope that our research and experimentation supports other organizations to get some good ideas. Like Experiment #1, this one has an initial budget of 5000 ADA, which is currently set aside to fund our community with the first version of the "gimbal" token. As this experiment progresses, we'll be sure to post our insights here.
+               <Paragraph>
+                   <a href="https://pooltool.io/pool/dddce8834df02109524aeb5afa0fd820fc29920eca91a63551ecc26a/epochs" className="classic">GimbalPool</a> is now registered on Cardano Mainnet - this is our first step in rolling out the gimbal token!
+               </Paragraph>
+               <Paragraph>
+                    Already, this experiment generated a lot of great conversations, and we're ready to test some ideas. Token-based incentive schemes should be taken seriously, and we hope that our research and experimentation supports other organizations to get some good ideas. Like Experiment #1, this one has an initial budget of 5000 ADA, which is currently set aside to fund our community with the first version of the "gimbal" token. You can <a href="https://gitlab.com/gimbalabs/initiatives/ex2-tokenomics-research" className="classic">see our latest thinking on GitLab</a>, and we'll be rolling out the first version of Gimbals over the next few weeks.
                 </Paragraph>
                 <TitleHeading3>Experiement #3: Translating Gimbalabs Content (launching March 8!)</TitleHeading3>
                 <Paragraph>
                     The next Gimbalabs Initiative launches on March 8, and our goal is to create systems for translating content so that word of Cardano reaches the widest possible audience. In the first two experiments we were reminded that even in this new, decentralized, autonomous world, it's as important as ever to have clear, actionable goals and that identifying project-leaders early helps. If you'd like to participate, be sure to look us up on Discord - and starting next week, check back here for further updates about all Gimbalabs Initiatives.
                 </Paragraph>
-                <DiscordButton />
             </section>
             
             <section>
