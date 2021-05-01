f2fc762071f241a6898109ad8a7271da98921e13
James Dunseith <jdunseith@gimbalabs.io>
2021-05-01 07:02:47 -0400

    UAdd weekly updates

--- pages/about/index.js
+++ pages/about/index.js
@@ -45,7 +45,7 @@ function about({videoList, teamList}) {
                         </ul>
                     </div>
                     <div className="col-span-1 w-full hidden md:contents">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/5sNPFW48htI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/CxpLaON1lKs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
 
