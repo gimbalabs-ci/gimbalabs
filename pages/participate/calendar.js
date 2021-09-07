768132c4e8272778388e552efb84643dde920e8d
james <james@pop-os.localdomain>
2021-09-07 08:58:09 -0400

    Add calendar feed

--- pages/participate/calendar.js
+++ pages/participate/calendar.js
@@ -6,6 +6,9 @@ export default function calendar() {
     return (
         <div className="w-full md:w-5/6 lg:w-3/4 mx-auto pb-20">
             <TitleHeading1>Participate: Gimbalabs Calendar</TitleHeading1>
+            <p class="my-4 text-purple-700 text-lg">
+                <a href="https://ics.teamup.com/feed/ksyq4sfs7367by71im/9336592.ics">Click here to subscribe to this Calendar via ics feed.</a>
+            </p>
             <section>
             <iframe src="https://teamup.com/ks359aocim5rmjch1o?title=Gimbalabs%20Calendar&showLogo=0&showSearch=0&showProfileAndInfo=0&showSidepanel=1&disableSidepanel=1&showTitle=0&showViewSelector=1&showMenu=0&showAgendaHeader=1&showAgendaDetails=0&showYearViewHeader=1" width="100%" height="800px" frameborder="0"></iframe>
             </section>
