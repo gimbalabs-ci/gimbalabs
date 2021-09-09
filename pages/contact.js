3e63b9fc2fac9fc14b308cfb7ed4ae221e40fb6e
james <james@pop-os.localdomain>
2021-09-09 11:56:49 -0400

    Contact form

--- pages/contact.js
+++ pages/contact.js
@@ -2,25 +2,21 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { TitleHeading1, TitleHeading2, TitleHeading3, HighlightText, CallOutText, Paragraph, CardHeading, CardText } from '../components/Type'
 import { DiscordButton } from '../components/Buttons'
+import { PopupButton } from '@typeform/embed-react'
 
 function contact() {
     return (
-        <div className="w-full md:w-5/6 lg:w-3/4 mx-auto md:py-20">
+        <div className="w-full md:w-3/4 lg:w-1/2 mx-auto md:py-20">
             
-                <TitleHeading2>Get in Touch</TitleHeading2>
-                <Paragraph>
-                    The best way to get in touch is by dropping by the Gimbalabs Discord server and saying hi.
-                </Paragraph>
-                <DiscordButton />
-
-                <Paragraph>
-                    If you prefer email, feel free to drop us a line here.
-                </Paragraph>
-
-                <Paragraph>
-                    Or if you'd like to inquire about our commercial services, we've got a form for that.
-                </Paragraph>                
-
+            <TitleHeading2>Get in Touch</TitleHeading2>
+            <Paragraph>
+                The best way to get in touch is by dropping by the Gimbalabs Discord server and saying hi. You can also fill out our Contact Form by clicking the link below.
+            </Paragraph>
+            <DiscordButton />
+            
+            <PopupButton id="Wy8sEWLe" style={{ fontSize: 20 }} className="mx-auto w-full bg-purple-500">
+                 Contact Form
+            </PopupButton>
         </div>
     )
 }
