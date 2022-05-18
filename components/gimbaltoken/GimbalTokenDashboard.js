c0bc8d8f79701793abba091df804585e9d3f2607
jamesdunseith <jdunseith@gimbalabs.io>
2022-05-17 21:39:47 -0400

    Initialize dashboard

---
+++ components/gimbaltoken/GimbalTokenDashboard.js
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React from "react";
+import PageTitleWithBorder from "../shared_ui/PageTitleWithBorder";
+
+export default function GimbalTokenDashboard() {
+    return (
+        <div className="mb-7 text-black2-900">
+            TEST TEST
+        </div>
+    );
+}
