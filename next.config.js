cc28e00f2bfa7382079fcafc8ebf3745dd157815
jamesdunseith <jdunseith@gimbalabs.io>
2022-10-10 12:58:38 -0400

    Change webpack config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: "empty",
+      resolve = {
+        fallback: {
+          fs: false
+        }
       };
     }
 
