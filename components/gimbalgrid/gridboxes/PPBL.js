8e2cfad8de612ddd0da40a3fe7241fb38845946e
jamesdunseith <jdunseith@gimbalabs.io>
2023-03-13 13:38:28 -0400

    Add PPBL link to homepage

--- components/gimbalgrid/gridboxes/PPBL.js
+++ components/gimbalgrid/gridboxes/PPBL.js
@@ -5,8 +5,8 @@ export default function PPBL() {
   return (
     <div className="h-full grid grid-cols-3 p-3 justify-center items-center">
       <div className="col-span-2 p-3">
-        <h1 className="text-2xl">Plutus Project-Based Learning 2023</h1>
-        <h2 className="py-1">New course starts 2023-03-13</h2>
+        <h1 className="text-2xl">Live Now: Plutus Project-Based Learning 2023</h1>
+        <h2 className="py-1"><a href="https://plutuspbl.io" target="_blank">Sign Up!</a></h2>
       </div>
     </div>
   );
