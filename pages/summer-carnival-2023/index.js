2921434762b23a54f703db05948070fe4e1a6d4b
jamesdunseith <jdunseith@gimbalabs.io>
2023-06-01 07:56:15 -0400

    Fix pages/summer-carnival-2023

--- pages/summer-carnival-2023/index.js
+++ pages/summer-carnival-2023/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../../components/Layouts/Layout";
-import CarnivalPage from "../../components/carnival/CarnivalPage";
+import CarnivalPage from "../../components/carnival/CarnivalPage.js";
 
 export default function Carnival() {
   return (
